ca64c64622d5b2ff950d93cfe0dc377d3a136655
RJPlog <44948239+RJPlog@users.noreply.github.com>
2022-12-01 14:59:35 +0100

    Update day2201_1_2.kt
    found a solution with one line

--- day01/kotlin/RJPlog/day2201_1_2.kt
+++ day01/kotlin/RJPlog/day2201_1_2.kt
@@ -1,6 +1,11 @@
 import java.io.File
 import kotlin.math.*
 
+// tag::oneliner[]
+fun day01Part1Solution() = File("day2201_puzzle_input.txt").readLines().joinToString("\n").split("\n\n").map {it.split("\n").map {it.toInt()}.sum()}.sortedDescending()[0]
+
+fun day01Part2Solution() = File("day2201_puzzle_input.txt").readLines().joinToString("\n").split("\n\n").map {it.split("\n").map {it.toInt()}.sum()}.sortedDescending().take(3).sum()
+// end::oneliner[]
 
 // tag::calories[]
 fun calories(in1: Int): Int {
@@ -23,12 +28,17 @@ fun calories(in1: Int): Int {
 	} else {
 		return caloriesPerElf.sorted().takeLast(3).sum()
 	}
-
 }
 // end::calories[]
 
 fun main() {
 	var t1 = System.currentTimeMillis()
+	
+	
+	var result: String =  File("day2201_puzzle_input.txt").readLines().joinToString("\n")  // = "1000\n2000\n3000\n\n4000\n5000\n6000\n\n7000\n8000\n9000\n10000"
+	var resultDay01 = result.split("\n\n").map {it.split("\n").map {it.toInt()}.sum()}.sortedDescending()[0]
+	println(resultDay01)
+    
 
 	var solution1 = calories(1)
 	var solution2 = calories(2)
@@ -40,11 +50,15 @@ fun main() {
 	println("*******************************")
 	println("Solution for part1")
 	println("   $solution1 is that Elf carrying?")
+	print("   oneliner: ")
+	println(day01Part1Solution())
 	println()
 // print solution for part 2
 	println("*******************************")
 	println("Solution for part2")
 	println("   $solution2 are those Elves carrying")
+	print("   oneliner: ")
+	println(day01Part2Solution())
 	println()
 // end::output[]
 
