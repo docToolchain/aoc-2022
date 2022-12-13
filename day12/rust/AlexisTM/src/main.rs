b21e6bc1792c2636c3ec63a631d2f1f310864cfa
Alexis Paques <alexis.paques@gmail.com>
2022-12-13 20:45:57 +0100

    Add solution day12

--- day12/rust/AlexisTM/src/main.rs
+++ day12/rust/AlexisTM/src/main.rs
@@ -1,6 +1,7 @@
-use std::fmt::Write;
-use std::fs;
-use std::{thread, time};
+use std::collections::VecDeque;
+
+use std::{fs};
+
 
 #[derive(Debug, Clone, PartialEq)]
 struct Pose {
@@ -11,19 +12,10 @@ struct Pose {
 #[derive(Debug, Clone)]
 struct Cell {
     pub altitude: u8,
-    pub steps_to_reach: u16,
+    pub seen: bool,
 }
 
 type Map = Vec<Vec<Cell>>;
-
-#[derive(Debug)]
-struct Problem {
-    pub heightmap: Map,
-    pub hiker: Pose,
-    pub destination: Pose,
-    pub width: i16,
-    pub height: i16,
-}
 
 #[derive(Debug, Clone)]
 struct Solution {
@@ -35,30 +27,30 @@ impl Solution {
     // Returns the 4 new solutions splitting from the current one, but filter them if:
     // - Out of boundsss
     // - Not reachable (too high/or low)
-    // - Not better than another concurrent solution
-    fn split(&self, problem: &mut Problem) -> Vec<Self> {
-        let mut vec = Vec::<Solution>::new();
+    // - Not seen than another concurrent solution
+    fn split(&self, problem: &mut Problem) -> VecDeque<Self> {
+        let mut vec = VecDeque::<Solution>::new();
         let mut other_step = self.clone();
         other_step.step += 1;
         let mut other = other_step.clone();
         other.pose.pos_height += 1;
-        if other.in_bounds(problem) && other.reachable(problem, self) && other.better(problem) {
-            vec.push(other);
+        if other.in_bounds(problem) && other.reachable(problem, self) && other.seen(problem) {
+            vec.push_back(other);
         }
         let mut other = other_step.clone();
         other.pose.pos_height -= 1;
-        if other.in_bounds(problem) && other.reachable(problem, self) && other.better(problem) {
-            vec.push(other);
+        if other.in_bounds(problem) && other.reachable(problem, self) && other.seen(problem) {
+            vec.push_back(other);
         }
         let mut other = other_step.clone();
         other.pose.pos_width += 1;
-        if other.in_bounds(problem) && other.reachable(problem, self) && other.better(problem) {
-            vec.push(other);
+        if other.in_bounds(problem) && other.reachable(problem, self) && other.seen(problem) {
+            vec.push_back(other);
         }
-        let mut other = other_step.clone();
+        let mut other = other_step;
         other.pose.pos_width -= 1;
-        if other.in_bounds(problem) && other.reachable(problem, self) && other.better(problem) {
-            vec.push(other);
+        if other.in_bounds(problem) && other.reachable(problem, self) && other.seen(problem) {
+            vec.push_back(other);
         }
         vec
     }
@@ -71,7 +63,7 @@ impl Solution {
         {
             return false;
         }
-        return true;
+        true
     }
 
     // Returns the altitude of the cell
@@ -83,88 +75,52 @@ impl Solution {
 
     // Returns true if we can reach it from our current altitude
     pub fn reachable(&self, problem: &Problem, from: &Solution) -> bool {
-        if self.altitude(problem).abs_diff(from.altitude(problem)) > 1 {
+        if self.altitude(problem) > from.altitude(problem) + 1 {
             return false;
         }
-        return true;
+        true
     }
 
-    // Returns flase if another solutions already reached this position with an equal or lower number of steps
-    pub fn better(&self, problem: &mut Problem) -> bool {
+    // Returns false if another solutions has already seen this one
+    pub fn seen(&self, problem: &mut Problem) -> bool {
         let cell = &mut problem.heightmap[usize::try_from(self.pose.pos_height).unwrap()]
             [usize::try_from(self.pose.pos_width).unwrap()];
-        if self.step >= cell.steps_to_reach {
-            return false;
+        if cell.seen {
+            false
+        } else {
+            cell.seen = true;
+            true
         }
-        cell.steps_to_reach = self.step;
-        return true;
     }
 }
 
-fn solve(problem: &mut Problem) -> Option<Solution> {
-    let mut solutions = Vec::<Solution>::new();
-    solutions.push(Solution {
-        pose: problem.hiker.clone(),
-        step: 0,
-    });
-
-    let mut steps = 0;
-    loop {
-        let mut next_solutions = Vec::<Solution>::new();
-
-        for solution in solutions.iter_mut() {
-            if solution.pose == problem.destination {
-                return Some(solution.clone());
-            }
-
-            next_solutions.append(&mut solution.split(problem));
-        }
-
-        // println!("Next solutions: {:?}", next_solutions);
-        if next_solutions.len() == 0 {
-            println!(
-                "No solution found :'( Last solutions ongoing were: {:?}",
-                solutions
-            );
-            return None;
-        }
-
-        fancy_print_problem_and_solutions(&problem, &next_solutions);
-        thread::sleep(time::Duration::from_millis(100));
-
-        drop(solutions);
-        solutions = next_solutions;
-        steps += 1;
-        // println!("Step: {}", steps);
-    }
+#[derive(Debug, Clone)]
+struct Problem {
+    pub heightmap: Map,
+    pub hiker: Pose,
+    pub destination: Pose,
+    pub start_part2: VecDeque<Solution>,
+    pub width: i16,
+    pub height: i16,
 }
 
-fn fancy_print_problem_and_solutions(problem: &Problem, solutions: &Vec<Solution>) {
-    let mut target_string = String::new();
-    for line_i in 0..problem.heightmap.len() {
-        for col_i in 0..problem.heightmap[line_i].len() {
-            let mut printed = false;
-            for solution in solutions {
-                if solution.pose.pos_height == line_i.try_into().unwrap()
-                    && solution.pose.pos_width == col_i.try_into().unwrap()
-                {
-                    write!(target_string, "o").unwrap();
-                    printed = true;
-                }
-            }
-            if !printed {
-                write!(target_string, "-").unwrap();
-            }
+fn solve(problem: &mut Problem, solutions: &mut VecDeque<Solution>) -> Option<Solution> {
+    while let Some(solution) = solutions.pop_front() {
+        if solution.pose == problem.destination {
+            return Some(solution.clone());
         }
-        write!(target_string, "\n").unwrap();
+
+        solutions.append(&mut solution.split(problem));
+        if solutions.is_empty() {
+            println!("FAILED: Last solution: {:?}", solution);
+        }
     }
-    write!(target_string, "\n").unwrap();
-    print!("{}", target_string);
+    None
 }
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Should have been able to read the file");
-    let width = input.lines().nth(0).unwrap().chars().count();
+    let width = input.lines().next().unwrap().chars().count();
     let height = input.lines().count();
 
     let mut problem = Problem {
@@ -179,6 +135,7 @@ fn main() {
         },
         width: width.try_into().unwrap(),
         height: height.try_into().unwrap(),
+        start_part2: VecDeque::<Solution>::new(),
     };
 
     // map[height][width]
@@ -188,7 +145,7 @@ fn main() {
             width,
             Cell {
                 altitude: 0,
-                steps_to_reach: u16::MAX,
+                seen: false,
             },
         );
     }
@@ -207,21 +164,41 @@ fn main() {
                             problem.hiker.pos_width = pos_width.try_into().unwrap();
                             problem.heightmap[pos_height][pos_width] = Cell {
                                 altitude: 0,
-                                steps_to_reach: 0,
+                                seen: false,
                             };
+                            problem.start_part2.push_back(Solution {
+                                pose: Pose {
+                                    pos_height: pos_height.try_into().unwrap(),
+                                    pos_width: pos_width.try_into().unwrap(),
+                                },
+                                step: 0,
+                            });
                         }
                         'E' => {
                             problem.destination.pos_height = pos_height.try_into().unwrap();
                             problem.destination.pos_width = pos_width.try_into().unwrap();
                             problem.heightmap[pos_height][pos_width] = Cell {
                                 altitude: 25,
-                                steps_to_reach: u16::MAX,
+                                seen: false,
                             };
                         }
-                        'a'..='z' => {
+                        'a' => {
+                            problem.start_part2.push_back(Solution {
+                                pose: Pose {
+                                    pos_height: pos_height.try_into().unwrap(),
+                                    pos_width: pos_width.try_into().unwrap(),
+                                },
+                                step: 0,
+                            });
                             problem.heightmap[pos_height][pos_width] = Cell {
-                                altitude: (altitude_char as u8) - ('a' as u8),
-                                steps_to_reach: u16::MAX,
+                                altitude: 0,
+                                seen: false,
+                            };
+                        }
+                        'b'..='z' => {
+                            problem.heightmap[pos_height][pos_width] = Cell {
+                                altitude: (altitude_char as u8) - b'a',
+                                seen: false,
                             };
                         }
                         _ => {}
@@ -233,8 +210,21 @@ fn main() {
     println!("Problem size: {}x{}", width, height);
     println!("Hiker: {:?}", &problem.hiker);
     println!("Best signal: {:?}", &problem.destination);
+    let mut problem_p2 = problem.clone();
 
-    let solution = solve(&mut problem);
+    let mut solutions_p1 = VecDeque::<Solution>::new();
+    solutions_p1.push_back(Solution {
+        pose: problem.hiker.clone(),
+        step: 0,
+    });
+    let solution = solve(&mut problem, &mut solutions_p1);
+    if let Some(solution) = solution {
+        println!("The solution: {:?}", solution);
+    }
+
+    let mut solutions_p2 = VecDeque::<Solution>::new();
+    solutions_p2.append(&mut problem.start_part2);
+    let solution = solve(&mut problem_p2, &mut solutions_p2);
     if let Some(solution) = solution {
         println!("The solution: {:?}", solution);
     }
