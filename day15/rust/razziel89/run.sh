b5182deac39ff91eea18762f3cf3a221af908cb8
Torsten Long <torsten.s.long@gmail.com>
2022-12-15 18:14:49 +0100

    Have run script exit after the first one finished

--- day15/rust/razziel89/run.sh
+++ day15/rust/razziel89/run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-MIN=0 MAX=1 ./target/release/aoc2022 &
-MIN=1 MAX=2 ./target/release/aoc2022 &
-MIN=2 MAX=3 ./target/release/aoc2022 &
-MIN=3 MAX=4 ./target/release/aoc2022 &
+trap 'kill "${pids[@]}"' EXIT
 
-wait
+pids=()
+MIN=0 MAX=1 cargo run --release &
+pids+=($!)
+MIN=1 MAX=2 cargo run --release &
+pids+=($!)
+MIN=2 MAX=3 cargo run --release &
+pids+=($!)
+MIN=3 MAX=4 cargo run --release &
+pids+=($!)
+
+# Wait for the first one to finish, then kill all of them.
+wait -n
