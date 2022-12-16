ba57b6bda7d84fe1301c36ac8c94c23ebee50869
Torsten Long <torsten.s.long@gmail.com>
2022-12-16 17:10:54 +0100

    Nonfunctional version of part 1

--- day16/rust/razziel89/src/main.rs
+++ day16/rust/razziel89/src/main.rs
@@ -73,6 +73,76 @@ fn pairwise_distances(
     result
 }
 
+fn backtrack(
+    valve_name_map: &HashMap<String, &data::Valve>,
+    distances: &HashMap<(String, String), usize>,
+    relevant_valves: &Vec<String>,
+    current_spot: &String,
+    current_time: usize,
+    max_time: usize,
+    current_best: usize,
+    visited: &mut HashSet<String>,
+    stack: &mut Vec<(String, usize)>,
+) -> Result<(usize, Vec<(String, usize)>)> {
+    let mut next_best = current_best;
+    let mut best_stack = stack.iter().map(|el| el.clone()).collect();
+
+    for next_spot in relevant_valves.iter() {
+        if visited.contains(next_spot) {
+            continue;
+        }
+
+        let time_spent_traveling = distances
+            .get(&(current_spot.to_string(), next_spot.to_string()))
+            .ok_or(Error::msg("cannot retrieve distance"))?;
+
+        // We add 1 because of the time it takes to open the valve.
+        let time_of_next_valve_opening = current_time + time_spent_traveling + 1;
+
+        // Check whether we have any time left to open another valve.
+        if time_of_next_valve_opening >= max_time {
+            // If not, we found the best we can using this route.
+            return Ok((current_best, best_stack));
+        } else {
+            // We will be able to open that valve.
+            // Remember that we visited it.
+            visited.insert(next_spot.to_string());
+            stack.push((next_spot.to_string(), time_of_next_valve_opening));
+
+            // Check by how much the next valve can increase our release value.
+            let next_valve_rate = valve_name_map
+                .get(next_spot)
+                .ok_or(Error::msg("cannot retrieve valve"))?
+                .rate;
+            let benefit = (max_time - time_of_next_valve_opening) * next_valve_rate;
+
+            let (possible_best, possible_stack) = backtrack(
+                valve_name_map,
+                distances,
+                relevant_valves,
+                next_spot,
+                time_of_next_valve_opening,
+                max_time,
+                current_best + benefit,
+                visited,
+                stack,
+            )?;
+
+            if possible_best > next_best {
+                next_best = possible_best;
+                best_stack = possible_stack;
+            }
+
+            // Forget that we visited the point. That's the backtracking bit.
+            visited.remove(next_spot);
+            stack.pop();
+        }
+    }
+
+    // In case we've already visited all the possible valves, return the best we found so far.
+    Ok((next_best, best_stack))
+}
+
 fn solve(file: &str) -> Result<()> {
     println!("PROCESSING {}", file);
 
@@ -83,18 +153,54 @@ fn solve(file: &str) -> Result<()> {
         None,
         None,
     )?;
-    let valve_map = valves
+    // We only want to look at valves that have non-zero rates for part 1. I suspect it's gonna be
+    // different for part 2.
+    let relevant_valves = valves
+        .iter()
+        .filter_map(|el| {
+            if el.rate > 0 {
+                Some(el.name.clone())
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    // for v in &valves {
+    //     println!("all {:?}", v);
+    // }
+    // for v in &relevant_valves {
+    //     println!("rel {:?}", v);
+    // }
+
+    // Compute pairwise distances.
+    let valve_name_map = valves
         .iter()
         .map(|el| (el.name.clone(), el))
         .collect::<HashMap<_, _>>();
+    let distances = pairwise_distances(&valve_name_map);
+    // println!("{:?}", distances);
 
-    for v in &valves {
-        println!("{:?}", v);
-    }
+    // Backtrack the solution.
+    let start_time = 0;
+    let max_time = 30;
+    let start = "AA".to_string();
+    let start_release = 0;
+    let mut visited = HashSet::<String>::with_capacity(relevant_valves.len());
+    let mut stack = vec![];
 
-    // Compute pairwise distances.
-    let distances = pairwise_distances(&valve_map);
-    println!("{:?}", distances);
+    let (max, best_stack) = backtrack(
+        &valve_name_map,
+        &distances,
+        &relevant_valves,
+        &start,
+        start_time,
+        max_time,
+        start_release,
+        &mut visited,
+        &mut stack,
+    )?;
+    println!("{:?} {}", best_stack, max);
 
     Ok(())
 }
