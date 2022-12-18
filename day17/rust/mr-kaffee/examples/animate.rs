0b8fff9db8b0e5051c26d6ad89308715853ad90e
Peter Wieland <peter@die-wielands.net>
2022-12-18 13:05:48 +0100

    Document animation
    Signed-off-by: Peter Wieland <peter@die-wielands.net>

--- day17/rust/mr-kaffee/examples/animate.rs
+++ day17/rust/mr-kaffee/examples/animate.rs
@@ -1,24 +1,34 @@
 use mr_kaffee_2022_17::{Chamber, RockInChamber};
-use std::{thread, time::Duration};
+use std::{env, thread, time::Duration};
 
 const CONTENT: &str = r#">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"#;
 
+// tag::main[]
 fn main() {
+    // accept a single numeric argumnet
+    let arg = env::args().skip(1).next();
+    let wait = arg
+        .map(|arg| arg.parse().expect("a positive amount of ms to pause"))
+        .unwrap_or(40);
+
     let mut chamber = Chamber::from(&CONTENT.into());
-    for _ in 0..2022 {
+    for k in 0..2022 {
         chamber.integrate_rock(|chamber, rock, x, y, w| {
             print!("\x1B[1;1H\x1B[J"); // clear console
             println!(
                 "{}",
-                RockInChamber::RIC {
+                RockInChamber {
                     chamber,
                     rock,
                     x,
                     y,
                     w,
+                    ..RockInChamber::default()
                 }
             );
-            thread::sleep(Duration::from_millis(20));
+            println!("Rock {}", k + 1);
+            thread::sleep(Duration::from_millis(wait));
         });
     }
 }
+// end::main[]
