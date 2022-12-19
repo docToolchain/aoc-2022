c7e3bb80a498ed284019abfa8ab8f1d9291c72f3
Torsten Long <torsten.s.long@gmail.com>
2022-12-19 17:54:46 +0100

    Try to speed up search

--- day19/rust/razziel89/src/data.rs
+++ day19/rust/razziel89/src/data.rs
@@ -81,64 +81,98 @@ impl State {
         // Only perform the drawn operation if we have enough resources for it. Otherwise,
         // implicitly perform a "nothing" operation.
         match act {
-            WhatToBuild::Nothing => {}
+            WhatToBuild::Nothing => {
+                Some(Self {
+                    time: self.time - 1,
+                    // Materials.
+                    ore: self.ore + self.ore_robots,
+                    clay: self.clay + self.clay_robots,
+                    obsidian: self.obsidian + self.obsidian_robots,
+                    geode: self.geode + self.geode_robots,
+                    // Robots.
+                    ore_robots: self.ore_robots,
+                    clay_robots: self.clay_robots,
+                    obsidian_robots: self.obsidian_robots,
+                    geode_robots: self.geode_robots,
+                })
+            }
             WhatToBuild::OreR => {
                 if self.ore < bp.ore_ore_cost {
-                    return None;
+                    None
+                } else {
+                    Some(Self {
+                        time: self.time - 1,
+                        // Materials.
+                        ore: self.ore + self.ore_robots - bp.ore_ore_cost,
+                        clay: self.clay + self.clay_robots,
+                        obsidian: self.obsidian + self.obsidian_robots,
+                        geode: self.geode + self.geode_robots,
+                        // Robots.
+                        ore_robots: self.ore_robots + 1,
+                        clay_robots: self.clay_robots,
+                        obsidian_robots: self.obsidian_robots,
+                        geode_robots: self.geode_robots,
+                    })
                 }
             }
             WhatToBuild::ClayR => {
                 if self.ore < bp.clay_ore_cost {
-                    return None;
+                    None
+                } else {
+                    Some(Self {
+                        time: self.time - 1,
+                        // Materials.
+                        ore: self.ore + self.ore_robots - bp.clay_ore_cost,
+                        clay: self.clay + self.clay_robots,
+                        obsidian: self.obsidian + self.obsidian_robots,
+                        geode: self.geode + self.geode_robots,
+                        // Robots.
+                        ore_robots: self.ore_robots,
+                        clay_robots: self.clay_robots + 1,
+                        obsidian_robots: self.obsidian_robots,
+                        geode_robots: self.geode_robots,
+                    })
                 }
             }
             WhatToBuild::ObsidianR => {
                 if self.ore < bp.obsidian_ore_cost || self.clay < bp.obsidian_clay_cost {
-                    return None;
+                    None
+                } else {
+                    Some(Self {
+                        time: self.time - 1,
+                        // Materials.
+                        ore: self.ore + self.ore_robots - bp.obsidian_ore_cost,
+                        clay: self.clay + self.clay_robots - bp.obsidian_clay_cost,
+                        obsidian: self.obsidian + self.obsidian_robots,
+                        geode: self.geode + self.geode_robots,
+                        // Robots.
+                        ore_robots: self.ore_robots,
+                        clay_robots: self.clay_robots,
+                        obsidian_robots: self.obsidian_robots + 1,
+                        geode_robots: self.geode_robots,
+                    })
                 }
             }
             WhatToBuild::GeodeR => {
                 if self.ore < bp.geode_ore_cost || self.obsidian < bp.geode_obsidian_cost {
-                    return None;
+                    None
+                } else {
+                    Some(Self {
+                        time: self.time - 1,
+                        // Materials.
+                        ore: self.ore + self.ore_robots - bp.geode_ore_cost,
+                        clay: self.clay + self.clay_robots,
+                        obsidian: self.obsidian + self.obsidian_robots - bp.geode_obsidian_cost,
+                        geode: self.geode + self.geode_robots,
+                        // Robots.
+                        ore_robots: self.ore_robots,
+                        clay_robots: self.clay_robots,
+                        obsidian_robots: self.obsidian_robots,
+                        geode_robots: self.geode_robots + 1,
+                    })
                 }
             }
         }
-        let mut next = Self {
-            time: self.time - 1,
-            // Materials.
-            ore: self.ore + self.ore_robots,
-            clay: self.clay + self.clay_robots,
-            obsidian: self.obsidian + self.obsidian_robots,
-            geode: self.geode + self.geode_robots,
-            ore_robots: self.ore_robots,
-            clay_robots: self.clay_robots,
-            obsidian_robots: self.obsidian_robots,
-            geode_robots: self.geode_robots,
-        };
-
-        match act {
-            WhatToBuild::Nothing => {}
-            WhatToBuild::OreR => {
-                next.ore_robots += 1;
-                next.ore -= bp.ore_ore_cost;
-            }
-            WhatToBuild::ClayR => {
-                next.clay_robots += 1;
-                next.ore -= bp.clay_ore_cost;
-            }
-            WhatToBuild::ObsidianR => {
-                next.obsidian_robots += 1;
-                next.ore -= bp.obsidian_ore_cost;
-                next.clay -= bp.obsidian_clay_cost;
-            }
-            WhatToBuild::GeodeR => {
-                next.geode_robots += 1;
-                next.ore -= bp.geode_ore_cost;
-                next.obsidian -= bp.geode_obsidian_cost;
-            }
-        }
-
-        Some(next)
     }
 }
 // end::data[]
