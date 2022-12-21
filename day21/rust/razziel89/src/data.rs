479c82cc7a2cc40dd718d39879d6565d66040e1b
Torsten Long <torsten.s.long@gmail.com>
2022-12-21 08:41:26 +0100

    Part 1

---
+++ day21/rust/razziel89/src/data.rs
@@ -0,0 +1,44 @@
+// tag::data[]
+use anyhow::{Error, Result};
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub struct Monkey {
+    pub name: String,
+    pub action: Action,
+}
+
+#[derive(Debug)]
+pub enum Action {
+    Shout(isize),
+    Op(String, String, char),
+}
+
+impl FromStr for Monkey {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s.split_whitespace().collect::<Vec<_>>().as_slice() {
+            [name, num] => Ok(Self {
+                name: name.trim_end_matches(":").to_string(),
+                action: Action::Shout(num.parse()?),
+            }),
+            [name, mon1, op, mon2] => {
+                if op.len() == 1 {
+                    Ok(Self {
+                        name: name.trim_end_matches(":").to_string(),
+                        action: Action::Op(
+                            mon1.to_string(),
+                            mon2.to_string(),
+                            op.chars().next().ok_or(Error::msg("empty op detected"))?,
+                        ),
+                    })
+                } else {
+                    Err(Error::msg("multi-char op detected"))
+                }
+            }
+            _ => Err(Error::msg("cannot parse monkey")),
+        }
+    }
+}
+// end::data[]
---
+++ day21/rust/razziel89/src/main.rs
@@ -0,0 +1,95 @@
+// Expected input file names.
+#![feature(let_chains, variant_count)]
+const SAMPLE1: &str = "sample.dat";
+const REAL: &str = "stage_1.dat";
+
+// Dependencies.
+extern crate derive_more;
+mod data;
+mod io;
+
+// tag::main[]
+use anyhow::{Error, Result};
+use std::collections::HashMap;
+// Constants.
+
+fn do_op(op: char, val1: &isize, val2: &isize) -> Result<isize> {
+    match op {
+        '+' => Ok(val1 + val2),
+        '-' => Ok(val1 - val2),
+        '*' => Ok(val1 * val2),
+        '/' => Ok(val1 / val2),
+        _ => Err(Error::msg("unknown op")),
+    }
+}
+
+fn solve(file: &str) -> Result<()> {
+    println!("PROCESSING {}", file);
+
+    // Read file and convert into data. We use a custom struct here just so we can continue using
+    // our parser function.
+    let monkeys = io::parse_chunks_to_data::<data::Monkey>(
+        io::read_lines_from_file(file, 1)?,
+        "monkey",
+        None,
+        None,
+    )?;
+
+    // A map from monkey names to their numbers for those monkeys that already know them.
+    let mut known = monkeys
+        .iter()
+        .filter_map(|el| {
+            if let data::Action::Shout(num) = el.action {
+                Some((el.name.clone(), num))
+            } else {
+                None
+            }
+        })
+        .collect::<HashMap<_, _>>();
+    // A map from monkey names to their ops for those monkeys that don't yet know their numbers.
+    let mut unknown = monkeys
+        .iter()
+        .filter_map(|el| {
+            if let data::Action::Op(mon1, mon2, op) = &el.action {
+                Some((el.name.clone(), (mon1, mon2, op)))
+            } else {
+                None
+            }
+        })
+        .collect::<HashMap<_, _>>();
+
+    let root_name = "root".to_string();
+    while unknown.contains_key(&root_name) {
+        let mut moved = vec![];
+        for (name, (mon1, mon2, &op)) in unknown.iter() {
+            if let Some(val1) = known.get(mon1.as_str()) {
+                if let Some(val2) = known.get(mon2.as_str()) {
+                    known.insert(name.clone(), do_op(op, val1, val2)?);
+                    moved.push(name.clone());
+                }
+            }
+        }
+        for now_known in moved {
+            unknown.remove(&now_known);
+        }
+    }
+
+    println!("{:?}\n", known.get(&root_name));
+
+    // println!("{:?}\n", monkeys);
+    // println!("{:?}\n", known);
+    // println!("{:?}\n", unknown);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    solve(SAMPLE1)?;
+    solve(REAL)?;
+
+    // solve(SAMPLE1, 10, 811_589_153)?;
+    // solve(REAL, 10, 811_589_153)?;
+
+    Ok(())
+}
+// end::main[]
