32e00b3d825f1ad3c5621d408ac2718c4689ef0a
Torsten Long <torsten.s.long@gmail.com>
2023-01-04 17:37:23 +0100

    Add time to node

--- day24/rust/razziel89/src/data.rs
+++ day24/rust/razziel89/src/data.rs
@@ -1,15 +1,17 @@
 // tag::data[]
 use anyhow::{Error, Result};
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 use std::str::FromStr;
 
-#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 pub struct Point {
     pub x: u8,
     pub y: u8,
     pub t: u16,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Direction {
     Up,
     Down,
@@ -17,11 +19,64 @@ pub enum Direction {
     Right,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Tile {
     Free,
     Wall,
     Blizzard(Direction),
+}
+
+pub struct Blizzard {
+    start: Point,
+    direction: Direction,
+    min_x: u8,
+    min_y: u8,
+    max_y: u16,
+    max_x: u16,
+}
+
+impl Blizzard {
+    pub fn new(start: Point, direction: Direction, min: Point, max: Point) -> Self {
+        Self {
+            start: Point {
+                x: start.x - min.x,
+                y: start.y - min.y,
+                t: 0,
+            },
+            direction,
+            min_x: min.x,
+            min_y: min.y,
+            max_x: (max.x - min.x) as u16,
+            max_y: (max.y - min.y) as u16,
+        }
+    }
+
+    pub fn at_time(&self, t: u16) -> Point {
+        match self.direction {
+            Direction::Up => Point {
+                x: self.start.x + self.min_x,
+                y: ((self.start.y as u16 + self.max_y - t % self.max_y) % self.max_y) as u8
+                    + self.min_y,
+                t,
+            },
+            Direction::Down => Point {
+                x: self.start.x + self.min_x,
+                y: ((self.start.y as u16 + t % self.max_y) % self.max_y) as u8 + self.min_y,
+                t,
+            },
+            Direction::Left => Point {
+                x: ((self.start.x as u16 + self.max_x - t % self.max_x) % self.max_x) as u8
+                    + self.min_x,
+                y: self.start.y + self.min_y,
+                t,
+            },
+            Direction::Right => Point {
+                x: ((self.start.x as u16 + t % self.max_x) % self.max_x) as u8 + self.min_x,
+                y: self.start.y + self.min_y,
+                t,
+            },
+        }
+    }
 }
 
 impl Point {
@@ -75,6 +130,19 @@ impl Point {
             },
         ]
     }
+
+    pub fn as_node(&self, time: Option<u16>) -> Node {
+        let mut result = Node { p: *self };
+        if let Some(t) = time {
+            result.p.t = t
+        }
+        result
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Node {
+    pub p: Point,
 }
 
 impl FromStr for Direction {
@@ -102,4 +170,43 @@ impl FromStr for Tile {
         }
     }
 }
+
+impl Node {
+    pub fn pos(&self) -> Point {
+        self.p
+    }
+
+    pub fn neighbours<'a>(&'a self) -> &'a HashSet<Point> {
+        unimplemented!("we cannot yet generate valid neighbour lists");
+    }
+
+    pub fn infinity_dist(&self, other: &Point) -> usize {
+        (other.x - self.p.x) as usize + (other.y - self.p.y) as usize
+    }
+
+    pub fn shift(&self, time: u16) -> Node {
+        Self {
+            p: Point {
+                x: self.p.x,
+                y: self.p.y,
+                t: time,
+            },
+        }
+    }
+}
+
+// We identify a node only by its position.
+impl Hash for Node {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.p.hash(state)
+    }
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.p == other.p
+    }
+}
+
+impl Eq for Node {}
 // end::data[]
--- day24/rust/razziel89/src/main.rs
+++ day24/rust/razziel89/src/main.rs
@@ -11,8 +11,91 @@ mod io;
 // tag::main[]
 use anyhow::{Context, Error, Result};
 use std::cmp::Ordering;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 // Constants.
+
+fn find_path<'a>(
+    start: &'a data::Node,
+    end: &'a data::Node,
+    graph: &'a HashSet<data::Node>,
+    estimator_fn: fn(&data::Node, &data::Point) -> usize,
+) -> Result<HashMap<data::Node, (Option<data::Point>, usize)>> {
+    let ref_point = end.pos();
+    let estimator = move |node: &data::Node| estimator_fn(node, &ref_point);
+    let get_node = move |node: &data::Point| {
+        graph
+            .get(&node.as_node(Some(0)))
+            .ok_or(Error::msg("node not found"))
+            .map(|el| el.shift(node.t))
+    };
+
+    let mut connections = HashMap::<data::Node, (Option<data::Point>, usize)>::new();
+    let mut checkable = HashMap::<data::Node, (data::Point, usize)>::new();
+
+    // Add starting point to resulting path.
+    connections.insert(start.clone(), (None, 0));
+    // Add neighbours of starting point to list of checkable values. Ignore neighbouring points
+    // that are not part of the graph.
+    for neigh in start.neighbours() {
+        let neigh_node = get_node(neigh)?;
+        // Estimated costs are the most direct possible connection plus 1, since every step costs
+        // one.
+        let estimate = estimator(&neigh_node);
+        checkable.insert(neigh_node, (start.pos(), estimate));
+        // connections.insert(neigh_node, (Some(start.pos()), 1));
+    }
+
+    // Search until we added the final node to the path or until there is nothing more to check.
+    while !connections.contains_key(end) && checkable.len() > 0 {
+        // Get node with minimum _estimated_ cost.
+        let next_best_node = checkable
+            .iter_mut()
+            // Get node with minimum estimated cost.
+            .min_by(|(_node1, (_pre1, cost1)), (_node2, (_pre2, cost2))| cost1.cmp(&cost2))
+            .ok_or(Error::msg("cannot find next node"))?
+            .0
+            .clone();
+        let (_, (predecessor, _old_estimate)) = checkable
+            .remove_entry(&next_best_node)
+            .ok_or(Error::msg("cannot find predecessor"))?;
+
+        let cost_of_predecessor = connections
+            .get(&predecessor.as_node(None))
+            .ok_or(Error::msg("predecessor has not been visited"))?
+            .1;
+
+        // Add point to resulting path unless we've found the end/
+        connections.insert(
+            next_best_node.clone(),
+            (Some(predecessor), cost_of_predecessor + 1),
+        );
+        // We've found the end. Add it in a hacky way.
+        if estimator(&next_best_node) == 0 {
+            connections.insert(
+                next_best_node.shift(end.p.t),
+                (Some(predecessor), cost_of_predecessor + 1),
+            );
+        }
+
+        // Add neighbours of point to list of checkable values.
+        for neigh in next_best_node.neighbours() {
+            let neigh_node = get_node(neigh)?;
+            if !connections.contains_key(&neigh_node) {
+                let estimate = cost_of_predecessor + estimator(&neigh_node);
+                let previous_best = checkable
+                    .get(&neigh_node)
+                    .unwrap_or(&(*neigh, std::usize::MAX))
+                    .1;
+                if previous_best > estimate {
+                    checkable.insert(neigh_node, (next_best_node.pos(), estimate));
+                }
+                // connections.insert(neigh_node, Some(start.pos()));
+            }
+        }
+    }
+
+    Ok(connections)
+}
 
 fn solve(file: &str) -> Result<()> {
     println!("PROCESSING {}", file);
@@ -22,8 +105,49 @@ fn solve(file: &str) -> Result<()> {
     let (occ_map, max_x, max_y) = io::parse_chars_to_data::<data::Tile>(file, "tile", None, None)?;
     let max = data::Point::new(max_x, max_y, 0);
 
-    println!("{:?}", occ_map);
-    println!("{:?}", max);
+    let start = occ_map
+        .iter()
+        .find(|(point, tile)| point.y == 0 && tile == &&data::Tile::Free)
+        .ok_or(Error::msg("cannot find start"))?
+        .0
+        .clone()
+        .as_node(None);
+
+    let end = occ_map
+        .iter()
+        .find(|(point, tile)| point.y == max.y && tile == &&data::Tile::Free)
+        .ok_or(Error::msg("cannot find end"))?
+        .0
+        .clone()
+        .as_node(None);
+
+    let graph = occ_map
+        .iter()
+        .filter_map(|(point, tile)| {
+            if tile != &data::Tile::Wall {
+                Some(point.as_node(None))
+            } else {
+                None
+            }
+        })
+        .collect::<HashSet<_>>();
+
+    let up_blizz = occ_map
+        .iter()
+        .filter_map(|(point, tile)| {
+            if let data::Tile::Blizzard(dir) = tile {
+                Some(point.as_node(None))
+            } else {
+                None
+            }
+        })
+        .collect::<HashSet<_>>();
+
+    let estimator = |node: &data::Node, point: &data::Point| node.infinity_dist(&point);
+
+    let path = find_path(&start, &end, &graph, estimator);
+
+    println!("{:?}", path);
 
     Ok(())
 }
